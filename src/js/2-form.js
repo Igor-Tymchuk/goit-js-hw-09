d704ab37b795f8adf4477a169396d0f663717539
Igor Tymchuk <167434833+Igor-Tymchuk@users.noreply.github.com>
2024-09-01 00:21:37 +0300

    2 tasks done!?

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -7,8 +7,9 @@ if(localStorage.getItem("feedback-form-state")) {
     const parseData = JSON.parse(localStorage.getItem("feedback-form-state"));
     form.elements.email.value = parseData.email;
     form.elements.message.value = parseData.message;
+    formData.email = parseData.email;
+    formData.message = parseData.message;
 }
-
 const saveData = (event) => {
     event.preventDefault();
     formData.email = event.currentTarget.elements.email.value.trim();
@@ -16,5 +17,20 @@ const saveData = (event) => {
     console.log("formData:", formData)
     localStorage.setItem("feedback-form-state", JSON.stringify(formData));
 }
-
-form.addEventListener("input", saveData)
+form.addEventListener("input", saveData);
+const submitData = (event) => {
+    event.preventDefault();
+    const parseData = JSON.parse(localStorage.getItem("feedback-form-state"));
+    if(!parseData.email || !parseData.message) {
+        alert("Fill please all fields")
+    }
+    else {
+        console.log(formData);
+        localStorage.clear();
+        formData.email = "";
+        formData.message = "";
+        form.elements.email.value = "";
+        form.elements.message.value = "";
+    }
+}
+form.addEventListener("submit", submitData);
